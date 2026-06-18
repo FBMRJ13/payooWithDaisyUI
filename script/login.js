bc6ece93409c00687e85aa5b156f2695d61e4d64
FBMRJ13 <mstmita5113@gmail.com>
2026-06-18 19:32:04 +0600

    first commit

---
+++ script/login.js
@@ -0,0 +1,16 @@
+document.getElementById("login-btn").addEventListener("click",function(){
+const phoneNumber=document.getElementById("phoNum");
+const Number=phoneNumber.value;
+
+
+const pin=document.getElementById("pin").value;
+if(Number=="01739586750" && pin=="1234"){
+    alert("login successful");
+    window.location.assign('/home.html')
+}
+else{
+    alert("login failed");
+    return;
+}
+
+})
